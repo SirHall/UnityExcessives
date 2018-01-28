6362430cac0a60e573503b17ae39a449d38c9958
SirHall <sirhallstein@gmail.com>
2018-01-28 14:58:47 +1000

    Removed incorrect dependecies, and added a SubArray extension method

--- C#/LinqE.cs
+++ C#/LinqE.cs
@@ -7,125 +7,147 @@ using System.Diagnostics;
 
 namespace Excessives.LinqE
 {
-	public static class LinqE
-	{
-		#region Loops
+    public static class LinqE
+    {
+        #region Loops
 
-		//Foreach, no return
-		public static IEnumerable<TSource> ForEach<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Action<TSource> action
-		)
-		{
-			for (int i = 0; i < enumerable.Count (); i++) {
-				action (enumerable.ElementAt (i));
-			}
-			return enumerable;
-		}
+        //Foreach, no return
+        public static IEnumerable<TSource> ForEach<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Action<TSource> action
+        )
+        {
+            for (int i = 0; i < enumerable.Count(); i++)
+            {
+                action(enumerable.ElementAt(i));
+            }
+            return enumerable;
+        }
 
-		//For, no return
-		public static IEnumerable<TSource> For<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Action<TSource, int> action
-		)
-		{
-			for (int i = 0; i < enumerable.Count (); i++) {
-				action (enumerable.ElementAt (i), i);
-			}
-			return enumerable;
-		}
+        //For, no return
+        public static IEnumerable<TSource> For<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Action<TSource, int> action
+        )
+        {
+            for (int i = 0; i < enumerable.Count(); i++)
+            {
+                action(enumerable.ElementAt(i), i);
+            }
+            return enumerable;
+        }
 
-		//Foreach, return
-		public static IEnumerable<TSource> ForEach<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Func<TSource, TSource> action
-		)
-		{
-			for (int i = 0; i < enumerable.Count (); i++) {
-				enumerable.ToArray () [i] = action (enumerable.ElementAt (i));
-			}
-			return enumerable.AsEnumerable ();
-		}
+        //Foreach, return
+        public static IEnumerable<TSource> ForEach<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Func<TSource, TSource> action
+        )
+        {
+            for (int i = 0; i < enumerable.Count(); i++)
+            {
+                enumerable.ToArray()[i] = action(enumerable.ElementAt(i));
+            }
+            return enumerable.AsEnumerable();
+        }
 
-		//For, return
-		public static IEnumerable<TSource> For<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Func<TSource, int, TSource> action
-		)
-		{
-			for (int i = 0; i < enumerable.Count (); i++) {
-				enumerable.ToArray () [i] = action (enumerable.ElementAt (i), i);
-			}
-			return enumerable.AsEnumerable ();
-		}
+        //For, return
+        public static IEnumerable<TSource> For<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Func<TSource, int, TSource> action
+        )
+        {
+            for (int i = 0; i < enumerable.Count(); i++)
+            {
+                enumerable.ToArray()[i] = action(enumerable.ElementAt(i), i);
+            }
+            return enumerable.AsEnumerable();
+        }
 
-		#endregion
+        #endregion
 
-		#region Loops Backward
+        #region Loops Backward
 
-		//Foreach, no return
-		public static IEnumerable<TSource> ForEachBack<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Action<TSource> action
-		)
-		{
-			for (int i = enumerable.Count () - 1; i >= 0; i--) {
-				action (enumerable.ElementAt (i));
-			}
-			return enumerable;
-		}
+        //Foreach, no return
+        public static IEnumerable<TSource> ForEachBack<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Action<TSource> action
+        )
+        {
+            for (int i = enumerable.Count() - 1; i >= 0; i--)
+            {
+                action(enumerable.ElementAt(i));
+            }
+            return enumerable;
+        }
 
-		//For, no return
-		public static IEnumerable<TSource> ForBack<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Action<TSource, int> action
-		)
-		{
-			for (int i = enumerable.Count () - 1; i >= 0; i--) {
-				action (enumerable.ElementAt (i), i);
-			}
-			return enumerable;
-		}
+        //For, no return
+        public static IEnumerable<TSource> ForBack<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Action<TSource, int> action
+        )
+        {
+            for (int i = enumerable.Count() - 1; i >= 0; i--)
+            {
+                action(enumerable.ElementAt(i), i);
+            }
+            return enumerable;
+        }
 
-		//Foreach, return
-		public static IEnumerable<TSource> ForEachBack<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Func<TSource, TSource> action
-		)
-		{
-			for (int i = enumerable.Count () - 1; i >= 0; i--) {
-				enumerable.ToArray () [i] = action (enumerable.ElementAt (i));
-			}
-			return enumerable.AsEnumerable ();
-		}
+        //Foreach, return
+        public static IEnumerable<TSource> ForEachBack<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Func<TSource, TSource> action
+        )
+        {
+            for (int i = enumerable.Count() - 1; i >= 0; i--)
+            {
+                enumerable.ToArray()[i] = action(enumerable.ElementAt(i));
+            }
+            return enumerable.AsEnumerable();
+        }
 
-		//For, return
-		public static IEnumerable<TSource> ForBack<TSource> (
-			this IEnumerable<TSource> enumerable,
-			Func<TSource, int, TSource> action
-		)
-		{
-			for (int i = enumerable.Count () - 1; i >= 0; i--) {
-				enumerable.ToArray () [i] = action (enumerable.ElementAt (i), i);
-			}
-			return enumerable.AsEnumerable ();
-		}
+        //For, return
+        public static IEnumerable<TSource> ForBack<TSource>(
+            this IEnumerable<TSource> enumerable,
+            Func<TSource, int, TSource> action
+        )
+        {
+            for (int i = enumerable.Count() - 1; i >= 0; i--)
+            {
+                enumerable.ToArray()[i] = action(enumerable.ElementAt(i), i);
+            }
+            return enumerable.AsEnumerable();
+        }
 
-		#endregion
+        #endregion
 
-		//		//Repeat, return
-		//		public static IEnumerable<TSource> RepeatReplace<TSource> (
-		//			this IEnumerable<TSource> enumerable,
-		//			int iterations,
-		//			Action<TSource, int> action
-		//		)
-		//		{
-		//			enumerable.
-		//
-		//			for (int i = 0; i < iterations; i++) {
-		//				action (enumerable.ElementAt (i), i);
-		//			}
-		//			return enumerable.AsEnumerable ();
-		//		}
-	}
+        #region Get Sub Array
+        public static IEnumerable<TSource> SubArray<TSource>(
+            this IEnumerable<TSource> enumerable,
+            int startIndex, int length
+        )
+        {
+            TSource[] final = new TSource[length];
+
+            Array.Copy(enumerable.ToArray(), startIndex, final, 0, length);
+
+            return final.AsEnumerable();
+        }
+        #endregion
+
+        //		//Repeat, return
+        //		public static IEnumerable<TSource> RepeatReplace<TSource> (
+        //			this IEnumerable<TSource> enumerable,
+        //			int iterations,
+        //			Action<TSource, int> action
+        //		)
+        //		{
+        //			enumerable.
+        //
+        //			for (int i = 0; i < iterations; i++) {
+        //				action (enumerable.ElementAt (i), i);
+        //			}
+        //			return enumerable.AsEnumerable ();
+        //		}
+    }
 }
